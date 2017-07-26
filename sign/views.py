98fb861a6343f5d919a6e849313c83ff35536a15
gaoxz <2285687708@qq.com>
2017-07-26 20:59:46 +0800

    event manage html

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 
 # Create your views here.
@@ -12,6 +12,8 @@ def login_action(request):
 		username = request.POST.get('username','')
 		password = request.POST.get('password','')
 		if username == 'admin' and password == 'admin123':
-			return HttpResponse('login success!')
+			return HttpResponseRedirect('/event_manage/')
 		else:
 			return render(request,'index.html',{'error':'username or password error!'})
+def event_manage(request):
+	return render(request,"event_manage.html")
