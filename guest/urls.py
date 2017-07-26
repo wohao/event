016d38eff56f5b3b489e0395ef10207beb30496f
gaoxz <2285687708@qq.com>
2017-07-26 13:29:53 +0800

    login

--- guest/urls.py
+++ guest/urls.py
@@ -20,4 +20,5 @@ from sign import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/$',views.index),
+    url(r'^login_action/$',views.login_action)
 ]
--- sign/views.py
+++ sign/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello Django!")
+	#return HttpResponse("Hello Django!")
+	return render(request,"index.html")
+
+def login_action(request):
+	if request.method == 'POST':
+		username = request.POST.get('username','')
+		password = request.POST.get('password','')
+		if username == 'admin' and password == 'admin123':
+			return HttpResponse('login success!')
+		else:
+			return render(request,'index.html',{'error':'username or password error!'})
