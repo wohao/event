ce33168e146688e4b76d2de70fefa005df89f4ed
gaoxz <2285687708@qq.com>
2017-07-27 13:45:49 +0800

    admin login

--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -11,12 +13,20 @@ def login_action(request):
 	if request.method == 'POST':
 		username = request.POST.get('username','')
 		password = request.POST.get('password','')
-		if username == 'admin' and password == 'admin123':
+		user = auth.authenticate(username=username,password=password)
+		if user is not None:
+			auth.login(request,user)
+
+			request.session['user'] = username
 			response = HttpResponseRedirect('/event_manage/')
-			response.set_cookie('user',username,3600)
+			#response.set_cookie('user',username,3600)
+			
 			return response
 		else:
 			return render(request,'index.html',{'error':'username or password error!'})
+
+@login_required
 def event_manage(request):
-	username = request.COOKIES.get('user', '')
+	#username = request.COOKIES.get('user', '')
+	username = request.session.get('user','')
 	return render(request,"event_manage.html",{"user":username})
